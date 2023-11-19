dd3f696d26192220e0151fbc2d9cde3ec16435cd
jaziraaaa <jazirabazarbekova52@gmail.com>
2023-11-19 18:13:25 +0600

    first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './index.html';
 import './index.scss';
-import { num, sum } from './modules/calc';
+import './scripts/modal.js'
 
 
 
-console.log(num(10,20));
-console.log(num(10,60));
-console.log(sum(10,20));
